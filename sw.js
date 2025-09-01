4ea9dab4819d475d3c537aa3ea01f080a6ae7db2
2pac2papi <acontec91@gmail.com>
2025-09-01 08:12:16 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,3 @@
-// PWA cache for GymLog Trainer
-const CACHE='gymlog-trainer-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))));});
+// PWA cache placeholder
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match('index.html'))));
