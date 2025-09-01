bd3325f7667f34d5738c6a89ec79daf9f2f18e98
2pac2papi <acontec91@gmail.com>
2025-08-31 21:12:50 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // PWA cache for GymLog Trainer
-const CACHE='gymlog-trainer-v1';
+const CACHE='gymlog-trainer-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
